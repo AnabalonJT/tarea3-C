d475ccaf96a95091e7b28ae10ebaee7608723e54
sepomas <jtanabalon@miuandes.cl>
2020-05-26 19:43:34 -0400

    initial commit
    nothihng

---
+++ tarea 3.c
@@ -0,0 +1,8 @@
+//
+//  tarea 3.c
+//  
+//
+//  Created by José Tomás Anabalon Diaz on 26-05-20.
+//
+
+#include "tarea 3.h"
---
+++ tarea 3.h
@@ -0,0 +1,13 @@
+//
+//  tarea 3.h
+//  
+//
+//  Created by Maria Ignacia Anabalon Diaz on 26-05-20.
+//
+
+#ifndef tarea_3_h
+#define tarea_3_h
+
+#include <stdio.h>
+
+#endif /* tarea_3_h */
