bf08fc71d3a5a9b16ce6fa38b075945756a80535
sepomas <jtanabalon@miuandes.cl>
2020-05-31 19:09:00 -0400

    ready2work

--- main.c
+++ main.c
@@ -4,7 +4,11 @@
 #include "card.h"
 
 int main (int argc, char* argv[]){
+    int nothing= argc; // son para que no me de error al compilar
+    char* palabra = argv[0];
+    printf("%d",nothing);
+    printf("%s",palabra);
 
-	PrintTest()
+    PrintTest();
 	return 0;
 }
