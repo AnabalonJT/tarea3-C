eefabfb7d52b612672185b4193de19ab6949f31d
SebastianGonzalezRP <38021173+SebastianGonzalezRP@users.noreply.github.com>
2020-05-31 18:33:06 -0400

    base tarea

---
+++ player.h
@@ -0,0 +1,10 @@
+typedef struct Player{
+		char nombre[20];
+		int exp;
+		int level;
+} Player;
+
+void PrintTest(){
+	printf("testn1\n");
+	
+}
--- tarea 3.c
+++
@@ -1,8 +0,0 @@
-//
-//  tarea 3.c
-//  
-//
-//  Created by José Tomás Anabalon Diaz on 26-05-20.
-//
-
-#include "tarea 3.h"
